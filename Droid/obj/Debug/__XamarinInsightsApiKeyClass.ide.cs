8ac44abcc5212c693f06241920006730c820427e
FRincon <frincon@healthconnex.com.au>
2016-03-22 13:45:17 +1100

    first commit

---
+++ Droid/obj/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace TestJenkins.Droid
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "967d7add5934972bbed4fe28bc706dffc93212b6";
+	}
+}
---
+++ iOS/obj/iPhoneSimulator/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace TestJenkins.iOS
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "967d7add5934972bbed4fe28bc706dffc93212b6";
+	}
+}
